e5468c9ed03ea52f6e438bd935831171692f778b
f14a2cd build service <f14a2cd+builds@gmail.com>
2020-01-18 22:40:14 +0000

    fx

--- export.php
+++ export.php
@@ -1,27 +1,21 @@
 <?php
 /* env: 0 - exec, 1 - apiid, 2 - apihash, 3 - server_port, 4 - socks_proxy */
-$excepted = ["api_id","api_hash"];
-if (!in_array($excepted, $_ENV)) {
+
+if (!isset($_SERVER['api_id'])||!isset($_SERVER['api_hash'])) {
 	echo('
-Usage: docker run telegramapidocker [args]
-Where args can be:
---api_id=(api_id) - Your API ID from my.telegram.org
---api_hash=(api_hash) - Your API hash from my.telegram.org
---listen=(listen port) - Port which you will listen it. This port should be correctly forwarded from docker
---socks=socks://(server):(port) - SOCKS proxy for connecting to Telegram (if applied)
+Usage: docker run -e api_id=(api_id from my.telegram.org) -e api_hash=(api_hash from my.telegram.org) telegramapidocker
+Also, can be used -e socks=socks://(user):(password)@(address):(port) for connect via socks.
+Username and password for proxy is not required if it\'s not set.
 ');
 exit(0);
 }
-foreach($_ENV as $args) {
-	foreach($excepted as $req) {
-		if (stripos($args,$req)) {
-			$export[$req] = str_replace("--{$req}=","",$args);
-		}
-	}
-}
+$export['api_id'] = $_SERVER['api_id'];
+$export['api_hash'] = $_SERVER['api_hash'];
+$export['listen'] = (isset($_SERVER['listen'])&&!empty($_SERVER['listen']))?$_SERVER['listen']:9537;
+$export['socks'] = (isset($_SERVER['socks'])&&!empty($_SERVER['socks']))?$_SERVER['socks']:'';
 
 if (!isset($export['listen'])) {echo "listen was not set. Setting it to '9537'".PHP_EOL; $export['listen'] = 9537;}
-if (isset($export['socks'])) {
+if (isset($export['socks'])&&!empty($export['socks'])) {
 	$socks = parse_url($export['socks']);
 	$export['telegram_proxy_address'] = $socks['host'];
 	$export['telegram_proxy_port'] = $socks['port'];
@@ -35,6 +29,15 @@ $output = '';
 foreach($export as $ename => $evalue) {
 	$output .= strtoupper($ename)."=".$evalue.PHP_EOL;
 }
+chdir("/telegram");
+$script_rev = "907f5c9b7233252f5238adbe99c9640fb2621384"; /* our rev */
+if (str_replace(array(PHP_EOL,"\r","\n","\r\n"),"",shell_exec("git rev-parse HEAD"))!==$script_rev) {
+	echo "Main git is out-of-date. Updating...".PHP_EOL;
+	shell_exec("git pull");
+}
+else {
+	echo "Everything is up-to-date".PHP_EOL;
+}
 file_put_contents("/telegram/.env",$output);
-echo "Saved configuration.";
+echo "Saved configuration.".PHP_EOL;
 exit(1);
